f16678285c0dc5717ce3b83f676e67655e0bf28a
marex980 <69250892+marex980@users.noreply.github.com>
2023-09-22 16:18:00 +0200

    01-NAVBAR

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,7 +13,16 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
     >
       <div className="absolute w-[50%] inset-0 gradient-01" />
-    navbar
+      <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+        <img 
+          src="/search.svg"
+          alt='search'
+          className="w-[24px] h-[24px] object-contain"
+          />
+          <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>
+            FSC WEBSITE
+          </h2>
+    </div>
   </motion.nav>
 );
 
